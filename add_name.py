b22dae04c7999bc89dc1380f84c0b6c3e5e36eee
Emaa19 <131900277+ImaneMottaqi@users.noreply.github.com>
2024-05-13 12:12:29 +0200

    Update add_name.py

--- add_name.py
+++ add_name.py
@@ -1,4 +1,4 @@
-import gspread
+"""import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 def verifier_et_ajouter_patient(nom):
@@ -29,3 +29,27 @@ def verifier_et_ajouter_patient(nom):
 nom_a_verifier = "Jean Dupont"
 resultat = verifier_et_ajouter_patient(nom_a_verifier)
 print(resultat)
+"""
+
+
+from fastapi import FastAPI
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+app = FastAPI()
+
+@app.post("/verifier_et_ajouter_patient/")
+def verifier_et_ajouter_patient_api(nom: str):
+    # Votre fonction vérification et ajout de patient
+    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]    
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    feuille_patients = client.open("caller's names").get_worksheet(1)
+    colonne_noms = feuille_patients.col_values(1)
+
+    if nom in colonne_noms:
+        return {"message": "Vous êtes déjà notre patient."}
+    else:
+        nouveau_patient = [nom]
+        feuille_patients.append_row(nouveau_patient)
+        return {"message": "Vous avez été ajouté avec succès dans notre base de données."}

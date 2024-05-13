5f99a2e4e0bdaba9b55c7ba0c7fe1dc5177e2946
Emaa19 <131900277+ImaneMottaqi@users.noreply.github.com>
2024-05-13 12:42:16 +0200

    Update add_name.py

--- add_name.py
+++ add_name.py
@@ -1,45 +1,16 @@
-"""import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-def verifier_et_ajouter_patient(nom):
-    # Configuration de l'authentification avec la clé API
-    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]    
-    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-    
-    # Autorisation du client avec les identifiants
-    client = gspread.authorize(creds)
-    
-    # Sélection de la feuille contenant les informations des patients (sheet2)
-    feuille_patients = client.open("caller's names").get_worksheet(1)  # Sélectionnez la deuxième feuille (index 1)
-    
-    # Récupérer toutes les valeurs de la colonne "Nom_Prenom"
-    colonne_noms = feuille_patients.col_values(1)  # Colonne 1 = Nom_Prenom
-    
-    # Vérifier si le nom est déjà présent dans la colonne "Nom_Prenom"
-    if nom in colonne_noms:
-        return "Vous êtes déjà notre patient."
-    else:
-        # Ajouter le nom à la liste des patients
-        nouveau_patient = [nom]
-        feuille_patients.append_row(nouveau_patient)  # Ajouter une nouvelle ligne avec le nom
-        
-        return "Vous avez été ajouté avec succès dans notre base de données."
-
-# Utilisation de la fonction pour vérifier et ajouter un patient
-nom_a_verifier = "Jean Dupont"
-resultat = verifier_et_ajouter_patient(nom_a_verifier)
-print(resultat)
-"""
-
-
 from fastapi import FastAPI
+from pydantic import BaseModel  # Importez BaseModel pour définir des modèles de données
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 app = FastAPI()
 
+# Définissez un modèle Pydantic pour représenter les données attendues dans le corps de la requête
+class PatientInput(BaseModel):
+    nom: str
+
 @app.post("/verifier_et_ajouter_patient/")
-def verifier_et_ajouter_patient_api(nom: str):
+def verifier_et_ajouter_patient_api(patient: PatientInput):
     # Votre fonction vérification et ajout de patient
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]    
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
@@ -47,9 +18,9 @@ def verifier_et_ajouter_patient_api(nom: str):
     feuille_patients = client.open("caller's names").get_worksheet(1)
     colonne_noms = feuille_patients.col_values(1)
 
-    if nom in colonne_noms:
+    if patient.nom in colonne_noms:
         return {"message": "Vous êtes déjà notre patient."}
     else:
-        nouveau_patient = [nom]
+        nouveau_patient = [patient.nom]
         feuille_patients.append_row(nouveau_patient)
         return {"message": "Vous avez été ajouté avec succès dans notre base de données."}
